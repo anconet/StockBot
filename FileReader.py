d4f10be125c7dde89e825bb0e185370aa533e488
Anconetani <Anthony_Anconetani@DELL.com>
2021-10-10 21:53:19 -0500

    Saving WOrk

--- FileReader.py
+++ FileReader.py
@@ -1,20 +1,19 @@
-print "FileReader"
+print("FileReader")
 
-testFile = open('TestData.txt','w');
+def exampleWritingAndReadingAFile():
+    testFile = open('TestData.txt', 'w')
 
-testFile.write("100\n");
-testFile.write("200\n");
-testFile.write("300\n");
-testFile.close();
+    testFile.write("100\n")
+    testFile.write("200\n")
+    testFile.write("300\n")
+    testFile.close()
 
-testFile = open('TestData.txt');
+    testFile = open('TestData.txt')
 
-testFileLine = testFile.readline();
-print(testFileLine,end='')
-testFileLine = testFile.readline();
-#print(testFileLine)
-#testFileLine = testFile.readline();
-#print(testFileLine)
-#while (testFileLine != ''):
-#    print(testFileLine)
-#    testFileLine = testFile.readline();
+    testFileLine = testFile.readline()
+    #print(testFileLine)
+    while testFileLine != '':
+        print(testFileLine,end='')
+        testFileLine = testFile.readline()
+
+exampleWritingAndReadingAFile()
